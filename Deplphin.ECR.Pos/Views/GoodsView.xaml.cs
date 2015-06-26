03dc9a6fc3e76c0fadfc9870029c7d74f47a7d4d
Pavel Boliybash <pavel.b@live.ru>
2015-06-26 23:53:30 +0300

    add2

---
+++ Deplphin.ECR.Pos/Views/GoodsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Deplphin.ECR.Pos.Views
+{
+    public partial class GoodsView : Page
+    {
+        public GoodsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
