927e3ea046b5641d459209ec7dfaad012a1fd1a2
Pavel Boliybash <pavel.b@live.ru>
2015-06-28 11:00:33 +0300

    ConfigurationManager И сохранение состояния.

---
+++ Deplphin.ECR.Pos/DAL/ConfigurationManager.cs
@@ -0,0 +1,81 @@
+﻿using Deplphin.ECR.Pos.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace Deplphin.ECR.Pos.DAL
+{
+    public class ConfigurationManager
+    {
+        private static ConfigurationManager configManager;
+
+        private ConfigurationManager()
+        {
+            GetGoodsFromFile(); // Загружаем товары из xml файла
+            GetGroupsFromFile(); // Загружаем Группы товаров из xml файла
+        }
+
+        public static ConfigurationManager GetInstance()
+        {
+            // для исключения возможности создания двух объектов 
+            // при многопоточном приложении
+            if (configManager == null)
+            {
+                lock (typeof(ConfigurationManager))
+                {
+                    if (configManager == null)
+                        configManager = new ConfigurationManager();
+                }
+            }
+
+            return configManager;
+        }
+
+
+        // Все товары
+        public ObservableCollection<Good> AllGoodsCollection { get; set; }
+
+        // Группы товаров
+        public ObservableCollection<GoodsGroup> GoodsGroupeCollection { get; set; }
+
+
+
+        public GoodsGroup SelectedGroup { get; set;}
+        public ObservableCollection<CheckItem> Check { get; set; }
+
+        private void GetGoodsFromFile()
+        {
+            AllGoodsCollection = new ObservableCollection<Good>();
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "Goods.xml"))
+            {
+                XmlSerializer myXmlSer = new XmlSerializer(typeof(ObservableCollection<Good>));
+                FileStream mySet = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "Goods.xml", FileMode.Open);
+                AllGoodsCollection = (ObservableCollection<Good>)myXmlSer.Deserialize(mySet);
+                mySet.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Файл: " + AppDomain.CurrentDomain.BaseDirectory + "Goods.xml" + " Необнаружен!");
+            }
+        }
+
+        private void GetGroupsFromFile()
+        {
+            GoodsGroupeCollection = new ObservableCollection<GoodsGroup>();
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "Groups.xml"))
+            {
+                XmlSerializer myXmlSer = new XmlSerializer(typeof(ObservableCollection<GoodsGroup>));
+                FileStream mySet = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "Groups.xml", FileMode.Open);
+                GoodsGroupeCollection = (ObservableCollection<GoodsGroup>)myXmlSer.Deserialize(mySet);
+                mySet.Close();
+            }
+            else
+            {
+                MessageBox.Show("Файл: " + AppDomain.CurrentDomain.BaseDirectory + "Groups.xml" + " Необнаружен!");
+            }
+        }
+    }
+}
--- Deplphin.ECR.Pos/ViewsModels/SalesViewModel.cs
+++ Deplphin.ECR.Pos/ViewsModels/SalesViewModel.cs
@@ -8,28 +8,46 @@ using System.IO;
 using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
+using Deplphin.ECR.Pos.DAL;
 
 namespace Deplphin.ECR.Pos.ViewsModels
 {
     public class SalesViewModel : ViewModelBase
     {
-
+        public ConfigurationManager cm;
         public SalesViewModel()
         {
-            GetGoodsFromFile(); // Загружаем товары из xml файла
-            GetGroupsFromFile(); // Загружаем Группы товаров из xml файла
-
+            cm = ConfigurationManager.GetInstance();
+            GoodsGroupeCollection = cm.GoodsGroupeCollection;
+            if(cm.SelectedGroup != null) SelectedGroup = cm.SelectedGroup;
+            if (cm.Check != null) Check = cm.Check;
 
             //SelectedGroup = GoodsGroupeCollection[0];
-
-
+            //goodsCollection = new ObservableCollection<Good>();
+            //goodsCollection.Add(new Good {Code=1,GoodsGroupeId=1,Name="Test1", Price=8.8 });
+            //goodsCollection.Add(new Good { Code = 2, GoodsGroupeId = 1, Name = "Test2", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 3, GoodsGroupeId = 1, Name = "Test3", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 4, GoodsGroupeId = 1, Name = "Test4", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 5, GoodsGroupeId = 2, Name = "Test5", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 6, GoodsGroupeId = 2, Name = "Test6", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 7, GoodsGroupeId = 2, Name = "Test7", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 8, GoodsGroupeId = 3, Name = "Test8", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 9, GoodsGroupeId = 3, Name = "Test9", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 10, GoodsGroupeId = 3, Name = "Test10", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 11, GoodsGroupeId = 3, Name = "Test11", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 12, GoodsGroupeId = 3, Name = "Test12", Price = 8.8 });
+            //goodsCollection.Add(new Good { Code = 13, GoodsGroupeId = 4, Name = "Test13", Price = 8.8 });
             //XmlSerializer myXmlSer = new XmlSerializer(GoodsCollection.GetType());
             //StreamWriter myWriter = new StreamWriter(Environment.CurrentDirectory + @"\Goods.xml");
             //myXmlSer.Serialize(myWriter, GoodsCollection);
             //myWriter.Close();
 
 
-
+            //GoodsGroupeCollection = new ObservableCollection<GoodsGroup>();
+            //GoodsGroupeCollection.Add(new GoodsGroup { Code = 1, Name = "Товары1" });
+            //GoodsGroupeCollection.Add(new GoodsGroup { Code = 2, Name = "Товары1" });
+            //GoodsGroupeCollection.Add(new GoodsGroup { Code = 3, Name = "Товары1" });
+            //GoodsGroupeCollection.Add(new GoodsGroup { Code = 4, Name = "Товары1" });
             //XmlSerializer myXmlSer = new XmlSerializer(GoodsGroupeCollection.GetType());
             //StreamWriter myWriter = new StreamWriter(Environment.CurrentDirectory + @"\Groups.xml");
             //myXmlSer.Serialize(myWriter, GoodsGroupeCollection);
@@ -37,7 +55,7 @@ namespace Deplphin.ECR.Pos.ViewsModels
 
         }
         // Все товары
-        public ObservableCollection<Good> AllGoodsCollection { get; private set; }
+        //public ObservableCollection<Good> AllGoodsCollection { get; private set; }
 
         // ТОвары выбранной группы
         private ObservableCollection<Good> goodsCollection;
@@ -65,6 +83,7 @@ namespace Deplphin.ECR.Pos.ViewsModels
             set 
             {
                 selectedGroup = value;
+                cm.SelectedGroup = value;
                 SetGoodsSelectedGrop(); // Выводит товары выбранной группы
                 RaisePropertyChanged(() => SelectedGroup);
                 
@@ -95,54 +114,24 @@ namespace Deplphin.ECR.Pos.ViewsModels
             set
             {
                 check = value;
+                cm.Check = value;
                 RaisePropertyChanged(() => Check);
             }
         } 
 
 
 
-        void GetGoodsFromFile()
-        {
-            AllGoodsCollection = new ObservableCollection<Good>();
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "Goods.xml"))
-            {
-                XmlSerializer myXmlSer = new XmlSerializer(typeof(ObservableCollection<Good>));
-                FileStream mySet = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "Goods.xml", FileMode.Open);
-                AllGoodsCollection = (ObservableCollection<Good>)myXmlSer.Deserialize(mySet);
-                mySet.Close();
-              
-            }
-            else
-            {
-                MessageBox.Show("Файл: " + AppDomain.CurrentDomain.BaseDirectory + "Goods.xml" + " Необнаружен!");
-            }
-        }
 
-        void GetGroupsFromFile()
-        {
-            GoodsGroupeCollection = new ObservableCollection<GoodsGroup>();
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "Groups.xml"))
-            {
-                XmlSerializer myXmlSer = new XmlSerializer(typeof(ObservableCollection<GoodsGroup>));
-                FileStream mySet = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "Groups.xml", FileMode.Open);
-                GoodsGroupeCollection = (ObservableCollection<GoodsGroup>)myXmlSer.Deserialize(mySet);
-                mySet.Close();
-            }
-            else
-            {
-                MessageBox.Show("Файл: " + AppDomain.CurrentDomain.BaseDirectory + "Groups.xml" + " Необнаружен!");
-            }
-        }
 
         void SetGoodsSelectedGrop()
         {
-            int count = AllGoodsCollection.Count;
+            int count = cm.AllGoodsCollection.Count;
             GoodsCollection = new ObservableCollection<Good>();
-            for (int i = 0; i <= count; i++)
+            for (int i = 0; i < count; i++)
             {
-                if (AllGoodsCollection[i].GoodsGroupeId == SelectedGroup.Code)
+                if (cm.AllGoodsCollection[i].GoodsGroupeId == SelectedGroup.Code)
                 {
-                    GoodsCollection.Add(AllGoodsCollection[i]);
+                    GoodsCollection.Add(cm.AllGoodsCollection[i]);
                 }
             }
         }
